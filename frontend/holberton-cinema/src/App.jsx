604d165dc4b2558a4eb36d193752c95b0125bf39
savvyh <7277@holbertonstudents.com>
2026-02-24 13:19:25 +0000

    feat: implement authentication logic in App component

--- frontend/holberton-cinema/src/App.jsx
+++ frontend/holberton-cinema/src/App.jsx
@@ -1,7 +1,36 @@
+import "./App.css"
+import { useEffect, useState } from "react"
+
 function App() {
-  return (
-    <div className="App">
-    </div>
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState("");
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    fetch('/api/auth/', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+      },
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.username) {
+        setIsLoggedIn(true);
+        setUserUsername(data.username);
+      }
+    })
+    .catch(error => {
+      console.error('Error auth:', error);
+    })
+  }, []);
+  
+  return isLoggedIn ?
+  (
+    <Dashboard />
+  ) : 
+  (
+    <Authentification />
   )
 }
 
