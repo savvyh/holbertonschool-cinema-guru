22ce521fb78fcf685b0daf72ab28029ea1e8656f
savvyh <7277@holbertonstudents.com>
2026-02-24 16:12:32 +0000

    feat: create a Dashboard component

---
+++ frontend/holberton-cinema/src/routes/dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import "./dashboard.css";
+import Header from "../../components/navigation/Header";
+
+export default function Dashboard({userUsername, setIsLoggedIn}) {
+    return (
+        <div>
+            <Header 
+                userUsername={userUsername}
+                setIsLoggedIn={setIsLoggedIn}
+            />
+        </div>
+    )
+}
