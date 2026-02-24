deefad3ef1120eddb4390277bc7c1d2271fb0993
savvyh <7277@holbertonstudents.com>
2026-02-24 15:57:11 +0000

    feat: implement authentication logic in Authentification component

--- frontend/holberton-cinema/src/routes/auth/Authentication.jsx
+++ frontend/holberton-cinema/src/routes/auth/Authentication.jsx
@@ -3,20 +3,48 @@ import Button from "../../components/general/Button";
 import Register from "./Register";
 import Login from "./Login";
 import { useState } from "react"
+import axios from 'axios';
 
 export default function Authentification({setIsLoggedIn, setUserUsername}) {
     const [_switch, _setSwitch] = useState(true);
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    function handleSubmit(event) {
+        event.preventDefault();
+        if (_switch) {
+            axios.post('/api/auth/login', {
+                username: username,
+                password: password
+            })
+            .then(response => {
+                const data = response.data;
+                localStorage.setItem('accessToken', data.token)
+                setIsLoggedIn(true);
+                setUserUsername(data.username);
+            })
+        } else {
+             axios.post('/api/auth/register', {
+                username: username,
+                password: password
+            })
+            .then(response => {
+                const data = response.data;
+                localStorage.setItem('token', data.token)
+                setIsLoggedIn(true);
+                setUserUsername(data.username);
+            })
+        }
+    }
+
     return (
         <>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <Button label="Sign In" onClick={() => _setSwitch(true)} />
                 <Button label="Sign Up" onClick={() => _setSwitch(false)} />
             </form>
 
-            {_switch ? 
+            {_switch ?
                 <Login 
                     username={username}
                     setUsername={setUsername}
@@ -28,7 +56,8 @@ export default function Authentification({setIsLoggedIn, setUserUsername}) {
                     setUsername={setUsername}
                     password={password}
                     setPassword={setPassword}
-                />}
+                />
+            }
         </>
     )
 }
