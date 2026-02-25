591e0bafbec8c085144b6040ac6b67bd603b5b76
savvyh <7277@holbertonstudents.com>
2026-02-25 10:18:10 +0000

    feat: add Sidebar component to Dashboard

--- frontend/holberton-cinema/src/routes/dashboard/Dashboard.jsx
+++ frontend/holberton-cinema/src/routes/dashboard/Dashboard.jsx
@@ -1,13 +1,15 @@
 import "./dashboard.css";
 import Header from "../../components/navigation/Header";
+import Sidebar from "../../components/navigation/SideBar";
 
-export default function Dashboard({userUsername, setIsLoggedIn}) {
-    return (
-        <div>
-            <Header 
-                userUsername={userUsername}
-                setIsLoggedIn={setIsLoggedIn}
-            />
-        </div>
-    )
+export default function Dashboard({ userUsername, setIsLoggedIn }) {
+  return (
+    <>
+      <div>
+        <Header userUsername={userUsername} setIsLoggedIn={setIsLoggedIn} />
+      </div>
+      
+      <Sidebar />
+    </>
+  );
 }
