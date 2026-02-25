116ab7573f667e1742b727bf247657fcda2b17f8
savvyh <7277@holbertonstudents.com>
2026-02-25 15:10:28 +0000

    feat: implement HomePage component with movie loading and filtering functionality

---
+++ frontend/holberton-cinema/src/routes/dashboard/HomePage.jsx
@@ -0,0 +1,62 @@
+import "./dashboard.css";
+import { useEffect, useState } from "react";
+import Filter from "../../components/movies/Filter";
+import MovieCard from "../../components/movies/MovieCard";
+import Button from "../../components/general/Button";
+import axios from "axios";
+
+export default function HomePage() {
+  const [movies, setMovies] = useState([]);
+  const [minYear, setMinYear] = useState(1970);
+  const [maxYear, setMaxYear] = useState(2022);
+  const [genres, setGenres] = useState([]);
+  const [sort, setSort] = useState("");
+  const [title, setTitle] = useState("");
+  const [page, setPage] = useState(1);
+
+  function loadMovies(page) {
+    axios
+      .get("/api/titles/advancedsearch", {
+        params: {
+          minYear: minYear,
+          maxYear: maxYear,
+          genres: genres,
+          title: title,
+          sort: sort,
+          page: page,
+        },
+      })
+      .then((response) => {
+        setMovies(response.data.movies);
+      });
+  }
+
+  useEffect(() => {
+    loadMovies(page);
+  }, [minYear, maxYear, genres, title, sort, page]);
+
+  return (
+    <div>
+      <Filter
+        minYear={minYear}
+        setMinYear={setMinYear}
+        maxYear={maxYear}
+        setMaxYear={setMaxYear}
+        genres={genres}
+        setGenres={setGenres}
+        sort={sort}
+        setSort={setSort}
+        title={title}
+        setTitle={setTitle}
+      />
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+      <Button
+        label="Load More.."
+        type="button"
+        onClick={() => loadMovies(page + 1)}
+      />
+    </div>
+  );
+}
