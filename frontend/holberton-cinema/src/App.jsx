7e9dac3de8ccd9523dda9358f663f346b9e9ed72
savvyh <7277@holbertonstudents.com>
2026-02-25 10:19:09 +0000

    refactor: clean up App component code and improve readability

--- frontend/holberton-cinema/src/App.jsx
+++ frontend/holberton-cinema/src/App.jsx
@@ -1,36 +1,47 @@
-import "./App.css"
-import { useEffect, useState } from "react"
-import axios from 'axios';
- 
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Dashboard from "./routes/dashboard/Dashboard";
+import Authentification from "./routes/auth/Authentication";
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userUsername, setUserUsername] = useState("");
 
   useEffect(() => {
-    const accessToken = localStorage.getItem('accessToken');
+    const accessToken = localStorage.getItem("accessToken");
 
-    axios.post('/api/auth/', {}, {
-      headers: {
-        'Authorization': `Bearer ${accessToken}`,
-      },
-    })
-    .then(response => {
-      const data = response.data;
-        setIsLoggedIn(true);
-        setUserUsername(data.username);
-    })
-    .catch(error => {
-      console.error('Error auth:', error);
-    })
+    if (accessToken) {
+      axios
+        .post(
+          "/api/auth/",
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          },
+        )
+        .then((response) => {
+          const data = response.data;
+          console.log("data :", data);
+          setIsLoggedIn(true);
+          setUserUsername(data.username);
+        })
+        .catch((error) => {
+          console.error("Error auth:", error);
+        });
+    }
   }, []);
-  
-  return isLoggedIn ?
-  (
+
+  return isLoggedIn ? (
     <Dashboard />
-  ) : 
-  (
-    <Authentification />
-  )
+  ) : (
+    <Authentification
+      setIsLoggedIn={setIsLoggedIn}
+      setUserUsername={setUserUsername}
+    />
+  );
 }
 
-export default App
+export default App;
