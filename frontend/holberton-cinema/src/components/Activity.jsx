5b574abed2858ba6f483685e24705de765ec0fff
savvyh <7277@holbertonstudents.com>
2026-02-26 11:03:41 +0000

    feat: update Activity component to display user activity details

--- frontend/holberton-cinema/src/components/Activity.jsx
+++ frontend/holberton-cinema/src/components/Activity.jsx
@@ -1,9 +1,25 @@
 import "./components.css";
 
-export default function Activity() {
+export default function Activity({ value }) {
   return (
-    <li>
-      <p></p>
+    <li className="activity-item">
+      <p>
+        <span className="activity-user">{value.user.username}</span>
+        <span className="activity-text"> added </span>
+        <span className="activity-title">{value.title.title}</span>
+        <span className="activity-text">
+          {" "}
+          {value.activityType === "favorite"
+            ? "to favorites"
+            : "to watch later"}{" "}
+          -{" "}
+          {new Date(value.createdAt).toLocaleDateString("fr-FR", {
+            month: "long",
+            day: "numeric",
+            year: "numeric",
+          })}
+        </span>
+      </p>
     </li>
   );
 }
